a7e819e2157798ab37876b3616101cbad110978c
mahmudulhasan-xs <98678062+mahmudulhasan-xs@users.noreply.github.com>
2022-08-25 14:44:30 +0600

    Update main.js

--- static/main.js
+++ static/main.js
@@ -8,10 +8,14 @@ var log = (mgs, ctx) => {
 
 var connectSocket = () => {
     let keyword = "elementor addon"
-    const socket = io(`http://localhost:3000?location=US&keyword=${keyword}`,{
+    const socket = io(`http://localhost:3000`,{
         extraHeaders:{
             "Site-Token":"ST01-eyJjaXBoZXJ0ZXh0IjoiU0RMcEgxbDJPcW9FZzNFS0hrVmg4OVhsT3o4ZUw5MUEwdGtsdko2WFFjZzFESnNBOHp2MjN5Z3dONUU3QVZtT0k5ZDZRbitFUG1FQTFpcjNQQ2hLMkF0eTlsRFhkMU5PakZsMTJNanVVajlqN0VNRDZIak14T3FIZjV5U0NUYTRGUWtWYWYwb0l4bGJYZFUwanhDdHdxTytNb1FhRUFEUGVFeEx4TTFRVzF4aElJelB1Wndqd050dzNNUkE1eFBMNXVcL0RrRkdBVXhtZ2pBNW5PWlhJM2c9PSIsIml2IjoiNDQ2NDZkNTdmMTAyMDk3ZWNhOTZkMTU0M2VjNGM5YWMiLCJzYWx0IjoiODAzOTRhN2Q0MDk1MGVkOTcyYjE4OTQyNzNmZDU4ODkiLCJpdGVyYXRpb25zIjo5OTl9",
             "Auth-Token":"AT01-eyJjaXBoZXJ0ZXh0IjoiQjdMZVwvZWNxbHVYRUZSNkZQN3F2TmY5VFhKMDZxejNjRzJyeWFUVWw4ZEtcL0tHWUlhTmVNUnNNalVOdkt5SVlYZWFGVlF2U1d3NzkwdXZiNFhGb1A2dnFoVWRnY25LTVRPeGFqNWJMUThucGNMZTUxdHNseE9cLzZaKzZPSG1YbVlHZkZ5QXdacndkMFU2M0t5dVVQdFdvUDJEcXUxK3AyaUdDRHVjOHJ0MnBjPSIsIml2IjoiMjQ4MGJlZmI3ZThhZTU5Y2Q4ZGQwYTU2Yjg2NzhmZDAiLCJzYWx0IjoiMDY2NWVlNjllNzhhYTQ0ZDRhYWY2OWNmYzBhNGUyYjYiLCJpdGVyYXRpb25zIjo5OTl9"
+        },
+        query:{
+            location:"US",
+            keyword: "elementor addon"
         }
     });
     // client-side
